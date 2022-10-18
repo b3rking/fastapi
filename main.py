2734bfa9204a72906fdef95669019ab408a9779d
b3rking <ishimweeschylebersy@gmail.com>
2022-10-18 20:56:59 +0200

    add some endpoints

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {"message": "Welcome to my api!"}
+
+@app.get('/posts')
+def posts():
+    return {"data": "this is a list of posts!"}
+
 
